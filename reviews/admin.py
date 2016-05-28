29c17bd864613998fd55f576fc54f1f62785b8a1
Marina Sheenkova <msheenkova@gmail.com>
2016-05-29 00:03:48 +0300

    added db and tables Product, Feature, ProductType

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews import models
+
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = (
+                    'id',
+                    'name',
+
+                    )
+
+
+class ProductFeatureInline(admin.TabularInline):
+    model = models.ProductFeature
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+                ProductFeatureInline,
+                ]
+    list_display = (
+                    'id',
+                    'type',
+                    'name',
+                    'description',
+                    )
+
+
+# class FeatureInline(admin.TabularInline):
+#     model = models.ProductType.features.through
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    # inlines = [
+    #     FeatureInline,
+    # ]
+    list_display = (
+                    'id',
+                    'name',
+
+                    )
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.ProductType, ProductTypeAdmin)
+admin.site.register(models.Feature, FeatureAdmin)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Feature(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=200)
+    features = models.ManyToManyField(Feature)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    features = models.ManyToManyField(Feature, through='ProductFeature')
+
+
+class ProductFeature(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    value = models.CharField(max_length=200)
